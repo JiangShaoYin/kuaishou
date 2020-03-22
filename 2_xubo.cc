3ecd1f371958624081e9f0efa8c4504e01084c7a
徐博 <xubo@interns.ainnovation.com>
2020-03-22 19:17:13 +0800

    1

--- 2_xubo.cc
+++ 2_xubo.cc
@@ -1,8 +1,27 @@
 #include <iostream>
-#include <map>
-#include <unordered_map>
+#include <algorithm>
+#include <vector>
+#include <stack>
 using namespace std;
-
 int main() {
-
+    int num;
+    vector<int> nums;
+    while (cin >> num) {
+        nums.push_back(num);
+    }
+    if (nums.size() < 2) {
+        cout << "-1" << endl;
+        return 0;
+    }
+    int maxNum = max(nums[0], nums[1]), secondNum = min(nums[0], nums[1]);
+    if (nums[0] > nums[1]) cout << "1 ";
+    for (int i = 2; i < nums.size(); i++) {
+        if (nums[i] > maxNum) {
+            secondNum = maxNum;
+            maxNum = nums[i];
+        } else if (nums[i] >= secondNum) {
+            cout << i << " ";
+            secondNum = nums[i];
+        }
+    }
 }
