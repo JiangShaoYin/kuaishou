28533ae7bcd845d32d3e26447252b87730530f37
徐博 <xubo@interns.ainnovation.com>
2020-03-22 19:19:34 +0800

    1

--- 2_xubo.cc
+++ 2_xubo.cc
@@ -14,14 +14,20 @@ int main() {
         return 0;
     }
     int maxNum = max(nums[0], nums[1]), secondNum = min(nums[0], nums[1]);
-    if (nums[0] > nums[1]) cout << "1 ";
+    int cnt = 0;
+    if (nums[0] > nums[1]) {
+        cout << "1 ";
+        cnt++;
+    }
     for (int i = 2; i < nums.size(); i++) {
-        if (nums[i] > maxNum) {
+        if (nums[i] >= maxNum) {
             secondNum = maxNum;
             maxNum = nums[i];
         } else if (nums[i] >= secondNum) {
             cout << i << " ";
             secondNum = nums[i];
+            cnt++;
         }
     }
+    if (cnt == 0) cout << "-1" << endl;
 }
